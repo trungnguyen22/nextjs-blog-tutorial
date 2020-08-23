3d0ef0e4002642f724e04a937ceed606d3397bf2
trungnguyen22 <johniusnguyen22297@gmail.com>
2020-08-23 19:07:58 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/post";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -22,11 +23,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
