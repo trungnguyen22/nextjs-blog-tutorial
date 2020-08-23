b18474746349f0ff390e6e979849d2faf2aa1c9b
trungnguyen22 <johniusnguyen22297@gmail.com>
2020-08-23 19:07:55 +0700

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,24 @@
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/post";
+import { getAllPostIds, getPostData } from "../../lib/post";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 
-export default function Post() {
-  return <Layout>{/*  */}</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -14,6 +30,11 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps() {
-  // Fetch necessary data for the blog post using param.id
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
